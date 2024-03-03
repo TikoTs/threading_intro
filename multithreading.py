4f981a77134e2c71cb547b37fc9b7f4788f4714b
TikoTs <tiko.tsakadze2018@gmail.com>
2024-03-04 01:29:50 +0400

    Initial commit

---
+++ multithreading.py
@@ -0,0 +1,40 @@
+import json
+import threading
+import time
+import requests
+import random
+
+f = open('products.json', 'w')
+
+
+lock = threading.Lock()
+products = []
+
+
+def get_response(start_range, end_range):
+    for i in range(start_range, end_range):
+        url = "https://dummyjson.com/products/" + str(i)
+        response = requests.get(url)
+        r = response.json()
+        with lock:
+            products.append(r)
+
+
+threads = []
+
+
+for i in range(0, 91, 10):
+    t = threading.Thread(target=get_response, args=(i + 1, i + 11))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+    time.sleep(random.randrange(0, 3))
+
+json.dump(products, f, indent=4)
+
+f.close()
+
+
+
